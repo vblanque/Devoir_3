40b637bf31cbca8fdbf457cfb9b9773915d79e73
colinDietrich <Colin.dietrich@polymtl.ca>
2024-04-16 14:08:08 -0400

    finished question 2

--- models.py
+++ models.py
@@ -75,7 +75,22 @@ class RegressionModel(object):
 
     def __init__(self) -> None:
         # Initialize your model parameters here
-        "*** TODO: COMPLETE HERE FOR QUESTION 2 ***"
+        self.batch_size = 10
+        self.learning_rate = 0.0005 * self.batch_size
+        self.n = 3
+        self.dim = 100
+        self.w = []
+        self.b = []
+        for i in range(self.n):
+            if(i==0):
+                self.w.append(nn.Parameter(1, self.dim))
+                self.b.append(nn.Parameter(1, self.dim))
+            elif(i==self.n-1):
+                self.w.append(nn.Parameter(self.dim, 1))
+                self.b.append(nn.Parameter(1, 1))
+            else:
+                self.w.append(nn.Parameter(self.dim, self.dim))
+                self.b.append(nn.Parameter(1, self.dim))
 
     def run(self, x: nn.Constant) -> nn.Node:
         """
@@ -86,7 +101,10 @@ class RegressionModel(object):
         Returns:
             A node with shape (batch_size x 1) containing predicted y-values
         """
-        "*** TODO: COMPLETE HERE FOR QUESTION 2 ***"
+        res = nn.ReLU(nn.AddBias(nn.Linear(x, self.w[0]), self.b[0]))
+        for i in range(1, self.n-1, 1):
+            res = nn.ReLU(nn.AddBias(nn.Linear(res, self.w[i]), self.b[i]))
+        return nn.AddBias(nn.Linear(res, self.w[self.n-1]), self.b[self.n-1])
 
     def get_loss(self, x: nn.Constant, y: nn.Constant) -> nn.Node:
         """
@@ -98,13 +116,30 @@ class RegressionModel(object):
                 to be used for training
         Returns: a loss node
         """
-        "*** TODO: COMPLETE HERE FOR QUESTION 2 ***"
+        predicted_y = self.run(x)
+        return nn.SquareLoss(predicted_y , y)
 
     def train(self, dataset: RegressionDataset) -> None:
         """
         Trains the model.
         """
-        "*** TODO: COMPLETE HERE FOR QUESTION 2 ***"
+        total_loss = 0
+        epoch_size = dataset.x.shape[0]
+        iteration = 0
+        for x, y in dataset.iterate_forever(self.batch_size):
+            iteration += 1
+            if iteration > epoch_size:
+                iteration = 1
+                total_loss = 0
+            loss = self.get_loss(x, y)
+            total_loss += nn.as_scalar(loss)
+            grad = nn.gradients(loss, self.w + self.b)
+            for i in range(self.n):
+                self.w[i].update(grad[i], -self.learning_rate)
+                self.b[i].update(grad[self.n + i], -self.learning_rate)
+
+            if iteration == epoch_size and total_loss / iteration < 0.02:
+                break
 
 
 class DigitClassificationModel(object):
